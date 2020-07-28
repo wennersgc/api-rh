02bd7012b99dcb5cd6f3924d8594e9f4fdaa1c30
wennersgc <wennersannerma@gmail.com>
2020-07-28 10:54:20 -0300

    Commit inicial

---
+++ src/main/java/com/api/rh/ApiRhApplication.java
@@ -0,0 +1,13 @@
+package com.api.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rh/ApiRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
