522d7976c67f3a9c37ece2ff311baaf356e47cb3
wennersgc <wennersannerma@gmail.com>
2020-08-03 15:07:23 -0300

    Refatoração dos models usando lombok

--- src/main/java/com/api/rh/model/Departamento.java
+++ src/main/java/com/api/rh/model/Departamento.java
@@ -1,11 +1,16 @@
 package com.api.rh.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
+@Getter
+@Setter
 @Entity(name = "departamento")
 public class Departamento {
 
@@ -13,22 +18,6 @@ public class Departamento {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String nomeDepto) {
-        this.descricao = nomeDepto;
-    }
 
     @Override
     public boolean equals(Object o) {
